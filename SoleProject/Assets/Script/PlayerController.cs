6999083c1fed52abeebb62a51beb410e5b246991
TheLeafeon <zkjx12@gmail.com>
2024-01-13 00:23:33 +0900

    Idle,Walk
    좌우이동 수정
    캐릭터 방향 전환
    
    --------------------------------
    움직임이 막히는 구간 확인 필요
    점프 제작 필요

--- SoleProject/Assets/Script/PlayerController.cs
+++ SoleProject/Assets/Script/PlayerController.cs
@@ -8,10 +8,10 @@ using UnityEngine.UIElements;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rigidbody2d;
-
+    SpriteRenderer spriteRenderer;
     Animator animator;
 
-    
+    Vector2 lookDirection = new Vector2(1, 0);
 
     //캐릭터 스테이터스
     public float playerSpeed = 0.0f;
@@ -19,7 +19,8 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
-
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
 
         
     }
@@ -27,14 +28,40 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //방향전환
+        if(Input.GetAxisRaw("Horizontal") != 0)
+        {
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //float vertical = Input.GetAxis("Vertical");
         float horizontal = Input.GetAxis("Horizontal");
-        UnityEngine.Debug.Log(horizontal);
+       
 
         Vector2 position = rigidbody2d.position;
 
-        position.x = position.x + playerSpeed * horizontal * Time.deltaTime;
+        Vector2 move = new Vector2(horizontal, rigidbody2d.velocity.y);
+
+        if (!Mathf.Approximately(move.x, 0.0f))
+        {
+            lookDirection.x = move.x;
+            
+            lookDirection.Normalize();
+        }
+        if(Mathf.Abs(move.x)>0.3f)
+        {
+            animator.SetBool("isWalk", true);
+        }
+        else
+        {
+            animator.SetBool("isWalk", false);
+        }
+
+        position = position + move * playerSpeed * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
-
     }
 }

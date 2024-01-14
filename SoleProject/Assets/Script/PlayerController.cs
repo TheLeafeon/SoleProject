f0341a66895723cf812c020d61ae1a907081834a
TheLeafeon <zkjx12@gmail.com>
2024-01-14 20:55:28 +0900

    Jump
    점프기능 추가

--- SoleProject/Assets/Script/PlayerController.cs
+++ SoleProject/Assets/Script/PlayerController.cs
@@ -11,10 +11,12 @@ public class PlayerController : MonoBehaviour
     SpriteRenderer spriteRenderer;
     Animator animator;
 
-    Vector2 lookDirection = new Vector2(1, 0);
+    //Vector2 lookDirection = new Vector2(1, 0);
 
     //캐릭터 스테이터스
     public float playerSpeed = 0.0f;
+    public float playerJumpPower = 0.0f;
+    private bool isFly = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,10 +31,20 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //방향전환
-        if(Input.GetAxisRaw("Horizontal") != 0)
+        if(Input.GetButton("Horizontal"))
         {
             spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
         }
+
+        //점프
+        if (Input.GetButtonDown("Jump") && !isFly)
+        {
+            isFly = true;
+            rigidbody2d.AddForce(Vector2.up * playerJumpPower, ForceMode2D.Impulse);
+            
+            UnityEngine.Debug.Log("Jump");
+        }
+
     }
 
     void FixedUpdate()
@@ -41,15 +53,15 @@ public class PlayerController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
        
 
-        Vector2 position = rigidbody2d.position;
+        //Vector2 position = rigidbody2d.position;
 
         Vector2 move = new Vector2(horizontal, rigidbody2d.velocity.y);
 
         if (!Mathf.Approximately(move.x, 0.0f))
         {
-            lookDirection.x = move.x;
+            //lookDirection.x = move.x;
             
-            lookDirection.Normalize();
+            //lookDirection.Normalize();
         }
         if(Mathf.Abs(move.x)>0.3f)
         {
@@ -60,8 +72,26 @@ public class PlayerController : MonoBehaviour
             animator.SetBool("isWalk", false);
         }
 
-        position = position + move * playerSpeed * Time.deltaTime;
+        rigidbody2d.velocity = new Vector2(move.x * playerSpeed, rigidbody2d.velocity.y);
 
-        rigidbody2d.MovePosition(position);
+
+        //점프 감지
+        if(rigidbody2d.velocity.y < 0.0f)
+        {
+            UnityEngine.Debug.DrawRay(rigidbody2d.position, Vector3.down, new Color(0, 1, 0));
+
+            RaycastHit2D rayHit = Physics2D.Raycast(rigidbody2d.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+            if (rayHit.collider != null)
+            {
+                if (rayHit.distance < 0.6f)
+                {
+                    UnityEngine.Debug.Log(rayHit.collider.name);
+                    isFly = false;
+                }
+            }
+        }
+            
+        
     }
 }

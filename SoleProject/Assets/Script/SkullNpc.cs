b6659db461c2fef47777ade9839a35c67ce445af
한상진 <zkjx12@gmail.com>
2024-04-04 02:52:07 +0900

    SkullNpc, SkullMonster_2
    SkullNpc와 대화 후 SkullMonster로 변신하는 기능 추가
    Npc방향 회전
    
    ------------------------------------------------------
    
    Stage_Skull 지형지물 제작 필요

--- SoleProject/Assets/Script/SkullNpc.cs
+++ SoleProject/Assets/Script/SkullNpc.cs
@@ -4,10 +4,37 @@ using UnityEngine;
 
 public class SkullNpc : MonoBehaviour
 {
+    SpriteRenderer spriteRenderer;
+    Animator animator;
+
+
+    //플레이어 위치 확인
+    public GameObject playerCharacter;
+
+    //스컬 몬스터
+    public GameObject skullMonster;
+
     public GameManager gameManager;
     //대화가 끝나는 타이밍
     public int skullNpcTalkEnd = 5;
     bool isTransform = false;
+
+    public bool transformEnd= false;
+
+    
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+
+        if(playerCharacter == null )
+        {
+            UnityEngine.Debug.Log("플레이어를 찾을 수 없음");
+        }
+        
+
+    }
 
 
     void Update()
@@ -16,7 +43,28 @@ public class SkullNpc : MonoBehaviour
         {
             isTransform = true ;
             UnityEngine.Debug.Log("변신");
+
+            animator.SetBool("isTransform", true);
         }
+
+        if(transform.position.x > playerCharacter.transform.position.x && spriteRenderer.flipX == false)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if(transform.position.x < playerCharacter.transform.position.x && spriteRenderer.flipX == true)
+        {
+            spriteRenderer.flipX = false;
+        }
+
+
+        if(transformEnd)
+        {
+            UnityEngine.Debug.Log("변신 완료");
+            skullMonster.SetActive(true);
+            Destroy(gameObject);
+        }
+
+
     }
 
 
